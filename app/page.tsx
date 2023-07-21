e591ebbc6056bb2199a26c4c3fb2fde76ac31bd9
dmitridrozdov <dm.drozdov@gmail.com>
2023-07-21 15:58:02 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-   <p className='text-6xl text-blue-500'>Hello saas AI</p>
+   <Button>Click me</Button>
   )
 }
