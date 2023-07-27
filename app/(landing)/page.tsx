9a7a31cc0dc633f2c6523329f27913d1e97df7e6
dmitridrozdov <dm.drozdov@gmail.com>
2023-07-27 13:39:39 +1000

    Update page.tsx

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 const LandingPage = () => {
   return (
-    <div>Landing page</div>
+    <div>
+      Landing page
+      <div>
+        <Link href='/sign-in'>
+          <Button>Log in</Button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
