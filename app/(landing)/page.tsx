78801cb468037cba0c2bf85a9ce1d94f64dc2d8b
dmitridrozdov <dm.drozdov@gmail.com>
2023-07-27 13:41:18 +1000

    Update page.tsx

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -9,6 +9,9 @@ const LandingPage = () => {
         <Link href='/sign-in'>
           <Button>Log in</Button>
         </Link>
+        <Link href='/sign-up'>
+          <Button>Register</Button>
+        </Link>
       </div>
     </div>
   )
