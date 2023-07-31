bbf3e5003b35dfd19e2a3e78b9e94e9bd8c3e04a
dmitridrozdov <dm.drozdov@gmail.com>
2023-07-31 15:37:21 +1000

    Update mobile-sidebar.tsx

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { Menu } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-// import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Sidebar } from "@/components/sidebar";
 
 export const MobileSidebar = ({
@@ -25,15 +25,15 @@ export const MobileSidebar = ({
   }
 
   return (
-    // <Sheet>
-    //   <SheetTrigger>
+    <Sheet>
+      <SheetTrigger>
         <Button variant="ghost" size="icon" className="md:hidden">
           <Menu />
         </Button>
-    //   </SheetTrigger>
-    //   <SheetContent side="left" className="p-0">
-    //     <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
-    //   </SheetContent>
-    // </Sheet>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
+      </SheetContent>
+    </Sheet>
   );
 };
