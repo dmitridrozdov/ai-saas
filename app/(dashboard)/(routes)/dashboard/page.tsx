96fde3b2827bb880d785fbbb24bfbfa9e179e407
dmitridrozdov <dm.drozdov@gmail.com>
2023-08-10 09:37:40 +1000

    Update page.tsx

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,27 +1,31 @@
 "use client";
 
-import { ArrowRight } from "lucide-react";
-import { useRouter } from "next/navigation";
-
-import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
+import { Montserrat } from 'next/font/google';
+const montserrat = Montserrat ({ weight: '300', subsets: ['latin'] });
 
-import { tools } from "@/constants";
+// import { ArrowRight } from "lucide-react";
+// import { useRouter } from "next/navigation";
+
+// import { Card } from "@/components/ui/card";
+// import { cn } from "@/lib/utils";
+
+// import { tools } from "@/constants";
 
 export default function HomePage() {
-  const router = useRouter();
+  // const router = useRouter();
 
   return (
     <div>
       <div className="mb-8 space-y-4">
-        <h2 className="text-2xl md:text-4xl font-bold text-center">
+        <h2 className={cn("text-2xl md:text-4xl font-bold text-center", montserrat.className)}>
           Explore the power of AI
         </h2>
-        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+        {/* <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
           Chat with the smartest AI - Experience the power of AI
-        </p>
+        </p> */}
       </div>
-      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+      {/* <div className="px-4 md:px-20 lg:px-32 space-y-4">
         {tools.map((tool) => (
           <Card onClick={() => router.push(tool.href)} key={tool.href} className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer">
             <div className="flex items-center gap-x-4">
@@ -35,7 +39,7 @@ export default function HomePage() {
             <ArrowRight className="w-5 h-5" />
           </Card>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
