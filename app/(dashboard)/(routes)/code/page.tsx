e9818b9ea8dfe06970737b16d8dbb9e203aba21c
dmitridrozdov <dm.drozdov@gmail.com>
2023-08-10 15:24:51 +1000

    Update page.tsx

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -99,7 +99,7 @@ const CodePage = () => {
                       <Input
                         className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                         disabled={isLoading} 
-                        placeholder="Simple toggle button using react hooks." 
+                        placeholder="" 
                         {...field}
                       />
                     </FormControl>
