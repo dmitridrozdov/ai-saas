344523a2c76a7d17d09ea4e1a44a9e22403a19a0
dmitridrozdov <dm.drozdov@gmail.com>
2023-08-20 19:21:55 +1000

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,41 +1,28 @@
-import { Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
+import { Code, ImageIcon, MessageSquare } from "lucide-react";
 
 export const MAX_FREE_COUNTS = 5;
 
 export const tools = [
   {
-    label: 'Conversation',
+    label: 'ChatGPT',
     icon: MessageSquare,
     href: '/conversation',
-    color: "text-violet-500",
-    bgColor: "bg-violet-500/10",
-  },
-  {
-    label: 'Music Generation',
-    icon: Music,
-    href: '/music',
-    color: "text-emerald-500",
-    bgColor: "bg-emerald-500/10",
-  },
-  {
-    label: 'Image Generation',
-    icon: ImageIcon,
-    color: "text-pink-700",
-    bgColor: "bg-pink-700/10",
-    href: '/image',
-  },
-  {
-    label: 'Video Generation',
-    icon: VideoIcon,
-    color: "text-orange-700",
-    bgColor: "bg-orange-700/10",
-    href: '/video',
+    color: "text-slate-400",
+    bgColor: "bg-white",
   },
   {
     label: 'Code Generation',
     icon: Code,
-    color: "text-green-700",
-    bgColor: "bg-green-700/10",
+    color: "text-slate-400",
     href: '/code',
+    bgColor: "bg-white",
   },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    color: "text-slate-400",
+    href: '/image',
+    bgColor: "bg-white",
+  },
+
 ];
