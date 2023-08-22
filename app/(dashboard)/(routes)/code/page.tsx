3c29659c79344ae5533bcc133028cfce92d536d3
dmitridrozdov <dm.drozdov@gmail.com>
2023-08-22 18:42:54 +1000

    Update page.tsx

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -98,7 +98,7 @@ const CodePage = () => {
                   <FormItem className="col-span-12 lg:col-span-10">
                     <FormControl className="m-0 p-0">
                       <Input
-                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        className={cn("border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent",montserrat.className)}
                         disabled={isLoading} 
                         placeholder="" 
                         {...field}
