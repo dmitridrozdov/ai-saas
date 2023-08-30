baa94ee6467f253ba842c98c950668ae32bcdd22
dmitridrozdov <dm.drozdov@gmail.com>
2023-08-30 11:26:59 +1000

    Update page.tsx

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -3,6 +3,10 @@ import { Settings } from "lucide-react";
 import { Heading } from "@/components/heading";
 import { SubscriptionButton } from "@/components/subscription-button";
 import { checkSubscription } from "@/lib/subscription";
+import { cn } from "@/lib/utils";
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat ({ weight: '300', subsets: ['latin'] });
 
 const SettingsPage = async () => {
   const isPro = await checkSubscription();
@@ -13,11 +17,11 @@ const SettingsPage = async () => {
         title="Settings"
         description="Manage account settings."
         icon={Settings}
-        iconColor="text-gray-700"
-        bgColor="bg-gray-700/10"
+        iconColor="text-slate-400"
+        bgColor="bg-white"
       />
       <div className="px-4 lg:px-8 space-y-4">
-        <div className="text-muted-foreground text-sm">
+        <div className={cn("text-muted-foreground text-sm", montserrat.className)}>
           {isPro ? "You are currently on a Pro plan." : "You are currently on a free plan."}
         </div>
         <SubscriptionButton isPro={isPro} />
