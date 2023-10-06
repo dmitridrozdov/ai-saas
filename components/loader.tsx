cb45f7e6c0b3a576b32fa0da28e73d6b332723f8
dmitridrozdov <dm.drozdov@gmail.com>
2023-10-06 19:25:02 +1100

    Update loader.tsx

--- components/loader.tsx
+++ components/loader.tsx
@@ -8,13 +8,14 @@ const montserrat = Montserrat ({ weight: '300', subsets: ['latin'] });
 export const Loader = () => {
   return (
     <div className="h-full flex flex-col gap-y-4 items-center justify-center">
-      <div className="w-10 h-10 relative animate-spin">
+      {/* <div className="w-10 h-10 relative animate-spin">
         <Image
           alt="Logo"
           src="/logo.png"
           fill
         />
-      </div>
+      </div> */}
+      <img src="/spinning-circles.svg" alt='loader' className='w-20 h-20 object-contain' />
       <p className={cn("text-sm text-muted-foreground", montserrat.className)}>
         AI is thinking...
       </p>
