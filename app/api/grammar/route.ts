b5531256de1e139cfa2355eba40e71f6838bcacc
dmitridrozdov <dm.drozdov@gmail.com>
2023-12-05 16:21:58 +1100

    Update route.ts

--- app/api/grammar/route.ts
+++ app/api/grammar/route.ts
@@ -38,7 +38,7 @@ export async function POST(
     }
 
     const freeTrial = await checkApiLimit();
-    const isPro = userId === 'user_2TE16TnzItwRJRtCvia1xCSxvdh' || (await checkSubscription());
+    const isPro = await checkSubscription();
 
     if (!freeTrial && !isPro) {
       return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
