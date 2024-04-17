10a78e35aebf43d1001d3de425754507312b9c25
dmitridrozdov <dm.drozdov@gmail.com>
2024-04-17 11:28:45 +1000

    Create route.ts

---
+++ app/api/grammargemini/route.ts
@@ -0,0 +1,41 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+export async function POST(
+  req: Request
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { messages  } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!messages) {
+      return new NextResponse("Messages are required", { status: 400 });
+    }
+
+    // console.log(messages[0].content)
+
+    const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+    const result = await model.generateContent('Correct English sentence and rephrase 3 time: ' + messages[0].content);
+    const response = await result.response;
+    const text = response.text();
+  
+    const json = {
+      role: 'assistant',
+      content: text,
+    };
+
+    return NextResponse.json(json);
+
+  } catch (error) {
+    console.log('[CODE_ERROR]', error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
