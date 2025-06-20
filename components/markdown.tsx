ebca5da0c211b5ebf8d14b93548504af204609e9
dmitridrozdov <dm.drozdov@gmail.com>
2025-06-20 16:16:16 +1000

    Update markdown.tsx

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -4,15 +4,21 @@ import DOMPurify from 'dompurify';
 
 type Props = {
   text: string;
+  className?: string; // Optional className prop
 };
 
-const md = markdownit({
-});
+const md = markdownit({ });
 
-const Markdown = ({ text }: Props) => {
+const Markdown = ({ text, className }: Props) => {
   const htmlcontent = md.render(text);
   const sanitized = DOMPurify.sanitize(htmlcontent);
-  return <div dangerouslySetInnerHTML={{ __html: sanitized }}></div>;
+  
+  return (
+    <div 
+      className={className}
+      dangerouslySetInnerHTML={{ __html: sanitized }}
+    />
+  );
 };
 
 export default Markdown;
