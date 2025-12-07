9bf6bf6504ec43efd5a3b1d7fb7cc92cfa0a92ed
dmitridrozdov <dm.drozdov@gmail.com>
2025-12-08 08:14:21 +1100

    Update route.ts

--- app/api/unittest/route.ts
+++ app/api/unittest/route.ts
@@ -49,7 +49,7 @@ export async function POST(req: Request) {
     const currentContent = fs.readFileSync(filePath, 'utf-8');
 
     const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
-    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash"});
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash"});
 
     const result = await model.generateContent(prompt + currentContent + "\`\`\`");
     const response = await result.response;
