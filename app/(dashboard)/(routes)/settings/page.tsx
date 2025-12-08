0228e4e55e22628586fd118ad21b744d4ded9c33
dmitridrozdov <dm.drozdov@gmail.com>
2025-12-09 08:11:21 +1100

    Update page.tsx

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -1,15 +1,15 @@
 import { Settings } from "lucide-react";
 
 import { Heading } from "@/components/heading";
-import { SubscriptionButton } from "@/components/subscription-button";
-import { checkSubscription } from "@/lib/subscription";
+// import { SubscriptionButton } from "@/components/subscription-button";
+// import { checkSubscription } from "@/lib/subscription";
 import { cn } from "@/lib/utils";
 import { Montserrat } from 'next/font/google';
 
 const montserrat = Montserrat ({ weight: '300', subsets: ['latin'] });
 
 const SettingsPage = async () => {
-  const isPro = await checkSubscription();
+  // const isPro = await checkSubscription();
 
   return ( 
     <div>
@@ -21,10 +21,10 @@ const SettingsPage = async () => {
         bgColor="bg-white"
       />
       <div className="px-4 lg:px-8 space-y-4">
-        <div className={cn("text-muted-foreground text-sm", montserrat.className)}>
+        {/* <div className={cn("text-muted-foreground text-sm", montserrat.className)}>
           {isPro ? "You are currently on a Pro plan." : "You are currently on a free plan."}
         </div>
-        <SubscriptionButton isPro={isPro} />
+        <SubscriptionButton isPro={isPro} /> */}
       </div>
     </div>
    );
