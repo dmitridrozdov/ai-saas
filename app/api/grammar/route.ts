fdf37379fdef96414d50e97a50857afe4469efe8
dmitridrozdov <dm.drozdov@gmail.com>
2025-12-10 16:13:24 +1100

    Update route.ts

--- app/api/grammar/route.ts
+++ app/api/grammar/route.ts
@@ -1,5 +1,3 @@
-// import { auth } from "@clerk/nextjs";
-import { getAuth } from '@clerk/nextjs/server';
 import { NextResponse } from "next/server";
 import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
 
@@ -19,13 +17,8 @@ export async function POST(
   req: Request
 ) {
   try {
-    const { userId } = getAuth(req as any);
     const body = await req.json();
     const { messages  } = body;
-
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
 
     if (!configuration.apiKey) {
       return new NextResponse("OpenAI API Key not configured.", { status: 500 });
@@ -35,21 +28,10 @@ export async function POST(
       return new NextResponse("Messages are required", { status: 400 });
     }
 
-    // const freeTrial = await checkApiLimit();
-    // const isPro = await checkSubscription();
-
-    // if (!freeTrial && !isPro) {
-    //   return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
-    // }
-
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [instructionMessage, ...messages]
     });
-
-    // if (!isPro) {
-    //   await incrementApiLimit();
-    // }
 
     return NextResponse.json(response.data.choices[0].message);
   } catch (error) {
