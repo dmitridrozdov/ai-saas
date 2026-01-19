8fe46d8f7f2ce042dc7c8d6b6b6009144310d622
dmitridrozdov <dm.drozdov@gmail.com>
2026-01-19 16:41:51 +1100

    Update mobile-sidebar.tsx

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -26,10 +26,8 @@ export const MobileSidebar = ({
 
   return (
     <Sheet>
-      <SheetTrigger>
-        <Button variant="ghost" size="icon" className="md:hidden">
-          <Menu />
-        </Button>
+      <SheetTrigger className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground h-10 w-10 md:hidden p-0">
+        <Menu className="h-5 w-5" />
       </SheetTrigger>
       <SheetContent side="left" className="p-0">
         <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
